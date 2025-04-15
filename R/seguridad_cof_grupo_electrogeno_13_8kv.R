e2265c3afed4d71e21a3167bff6cb88dc92af9aa
Joaquín Caicedo Navarro <joacocn@gmail.com>
2025-04-15 13:05:49 -0500

    Actualización tablas y cof seguridad GE

--- R/seguridad_cof_grupo_electrogeno_13_8kv.R
+++ R/seguridad_cof_grupo_electrogeno_13_8kv.R
@@ -1,35 +1,33 @@
-#' @title Safety cost of Failure for Transformer
-#' @description This function calculates safety consequences of failure
-#' (cf. section 7.3, page 79, CNAIM, 2021). Safety consequences
-#' of failure is used in
-#' the derivation of consequences of failure see \code{\link{cof}}().
-#' @param tf_asset_category String The type of Transformer asset category
-#' Options: \code{tf_asset_category = c("6.6/11kV Transformer (GM)",
-#' "20kV Transformer (GM)", "33kV Transformer (GM)", "66kV Transformer (GM) "
-#' "132kV Transformer (GM) ")}.
-#' @param location_risk String Type of location risk factor criteria for Transformer
-#' (cf. section D.2.2, page 183, CNAIM, 2021).
-#' Options: \code{location_risk = c("Low", "Medium", "High")}.
-#' The default setting is
-#' \code{location_risk = "Medium"}.
-#' @param type_risk String Type of type of risk rating criteria for Transformer
-#' (cf. section D.2.2, page 183, CNAIM, 2021)
-#' Options: \code{type_risk = c("Low", "Medium", "High")}.
-#' The default setting is
-#' \code{type_risk = "Medium"}.
-#' @param gb_ref_given optional parameter to use custom reference values
-#' @return Numeric. Safety consequences of failure for Transformers
+#' @title Costo de seguridad por falla para grupo electrógeno diésel
+#' @description Esta función calcula las consecuencias de seguridad por falla
+#' (cf. sección 7.3, página 79, CNAIM, 2021). Las consecuencias de seguridad
+#' por falla se utilizan en la derivación de las consecuencias de falla, ver 
+#' \code{\link{cof}}().
+#' @param tipo_generador Texto. El tipo de categoría de activo.
+#' Opciones: \code{tipo_generador = c("Grupo Electrógeno Diésel 13.8kV")}.
+#' @param riesgo_ubicación Texto. Tipo de criterio de factor de riesgo de ubicación 
+#' para grupo electrógeno diésel #' (cf. sección D.2.2, página 183, CNAIM, 2021).
+#' Opciones: \code{riesgo_ubicación = c("Bajo", "Medio", "Alto")}.
+#' La configuración predeterminada es \code{riesgo_ubicación = "Medio"}.
+#' @param riesgo_tipo Texto. Tipo de criterio de calificación de riesgo 
+#' para grupo electrógeno diésel (cf. sección D.2.2, página 183, CNAIM, 2021).
+#' Opciones: \code{riesgo_tipo = c("Bajo", "Medio", "Alto")}.
+#' La configuración predeterminada es \code{riesgo_tipo = "Medio"}.
+#' @param gb_ref_given Parámetro opcional para usar valores de referencia personalizados.
+#' @return Numérico. Consecuencias de seguridad por falla para grupos electrógenos diésel
 #' @source DNO Common Network Asset Indices Methodology (CNAIM),
 #' Health & Criticality - Version 2.1, 2021:
 #' \url{https://www.ofgem.gov.uk/sites/default/files/docs/2021/04/dno_common_network_asset_indices_methodology_v2.1_final_01-04-2021.pdf}
+#' Y adaptación de CNAIM para considerar grupos electrógenos diésel.
 #' @export
 #' @examples
-#' safety_cof_transformers(tf_asset_category = "33kV Transformer (GM)",
-#' location_risk = "Default",
-#' type_risk = "Default")
-seguridad_cof_grupo_electrogeno_13_8kv <- function(tf_asset_category,
-                                                   location_risk,
-                                                   type_risk,
+#' seguridad_cof_grupo_electrogeno_13_8kv(tipo_generador = "Grupo Electrógeno Diésel 13.8kV",
+#'                                        riesgo_ubicación = "Default",
+#'                                        riesgo_tipo = "Default")
+
+seguridad_cof_grupo_electrogeno_13_8kv <- function(tipo_generador = "Grupo Electrógeno Diésel 13.8kV",
+                                                   riesgo_ubicación,
+                                                   riesgo_tipo,
                                                    gb_ref_given = NULL){
   `Asset Register Category` = `Health Index Asset Category` = `Asset Category` = NULL
 
@@ -41,29 +39,29 @@ seguridad_cof_grupo_electrogeno_13_8kv <- function(tf_asset_category,
   }
 
   asset_category <- gb_ref_taken$categorisation_of_assets %>%
-    dplyr::filter(`Asset Register Category` == tf_asset_category) %>%
+    dplyr::filter(`Asset Register Category` == tipo_generador) %>%
     dplyr::select(`Health Index Asset Category`) %>% dplyr::pull()
 
-  reference_costs_of_failure_tf <- dplyr::filter(gb_ref_taken$reference_costs_of_failure,
-                                                 `Asset Register Category` ==
-                                                   tf_asset_category)
+  reference_costs_of_failure_GE <- dplyr::filter(gb_ref_taken$reference_costs_of_failure,
+                                                 `Asset Register Category` == tipo_generador)
 
   # Reference financial cost of failure -------------------------------------
-  scost <- reference_costs_of_failure_tf$`Safety - (GBP)`
+  scost <- reference_costs_of_failure_GE$`Safety - (GBP)`
 
-  if (location_risk == "Default") location_risk <- "Medium (Default)"
-  if (location_risk == "Medium") location_risk <- "Medium (Default)"
-  if (type_risk == "Default") type_risk <- "Medium"
+  if (riesgo_ubicación == "Default") riesgo_ubicación <- "Medio (Default)"
+  if (riesgo_ubicación == "Medio") riesgo_ubicación <- "Medio (Default)"
+  if (riesgo_tipo == "Default") riesgo_tipo <- "Medio (Default)"
+  if (riesgo_tipo == "Medio") riesgo_tipo <- "Medio (Default)"
 
-  safety_conseq_factor_sg_tf_oh <- gb_ref_taken$safety_conseq_factor_sg_tf_oh
+  factor_consec_seg_generador <- gb_ref_taken$factor_consec_seg_generador
 
-  row_no <- which(safety_conseq_factor_sg_tf_oh$
-                    `Safety Consequence Factor - Switchgear, Transformers & Overhead Lines...2` ==
-                    location_risk)
+  row_no <- which(factor_consec_seg_generador$
+                  `Factor de consecuencia de seguridad - Generadores...2` ==
+                  riesgo_ubicación)
 
-  col_no <- grep(type_risk, colnames(safety_conseq_factor_sg_tf_oh))
+  col_no <- grep(riesgo_tipo, colnames(factor_consec_seg_generador))
 
-  safety_consequence_factor <- safety_conseq_factor_sg_tf_oh[row_no, col_no]
+  safety_consequence_factor <- factor_consec_seg_generador[row_no, col_no]
 
   # Safety consequence of failure -------------------------------------------
   safety_cof <- safety_consequence_factor * scost
