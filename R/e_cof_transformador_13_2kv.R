b75b8c45b5162f25a8453b36cd452f584c1c47f4
Joaquín Caicedo Navarro <joacocn@gmail.com>
2025-04-23 17:26:42 -0500

    Actualización de la función e_cof_transformador_13_2kv: traducción de comentarios y parámetros al español, y ajuste de nombres de variables para reflejar el transformador de 13.2 kV.

--- R/e_cof_transformador_13_2kv.R
+++ R/e_cof_transformador_13_2kv.R
@@ -1,41 +1,38 @@
 #' @importFrom magrittr %>%
-#' @title Environmental Consequences of Failure for transformers
-#' @description This function calculates environmental consequences of failure
-#' for all type of transformers.
-#' (cf. section 7.5, page 84, CNAIM, 2021). Environmental consequences
-#'  of failure is used in
-#' the derivation of consequences of failure see \code{\link{cof}}().
-#' @param asset_type_tf String. Transformer types.
-#' Options:
-#' \code{asset_type_tf = c("6.6/11kV Transformer (GM)",
-#' "20kV Transformer (GM)", "33kV Transformer (GM)",
-#' "66kV Transformer (GM)", "132kV Transformer (GM)")}.
-#' @param rated_capacity Numeric. The rated capacity for a transformer. For type
-#' \code{"6.6/11kV Transformer (GM)"} and \code{"20kV Transformer (GM)"} use kVA
-#' ratings. For \code{"20kV Transformer (GM)", "33kV Transformer (GM)",
-#' "66kV Transformer (GM)", "132kV Transformer (GM)"} use MVA ratings.
-#' A setting of \code{"Default"} will result in a
-#' size environmental factor of 1 (cf. table 230, page 187, CNAIM, 2021).
-#' @param prox_water Numeric. Specify the proximity to a water course in meters.
-#' A setting of \code{"Default"} will result in a proximity factor of 1. Thus
-#' assume the proximity to a water course is between 80m and 120m
-#' (cf. table 231, page 188, CNAIM, 2021).
-#' @param bunded String. Options: \code{bunded = c("Yes", "No", "Default")}.
-#' A setting of \code{"Default"} will result in a bunding factor of 1.
-#' @param gb_ref_given optional parameter to use custom reference values
-#' @return Numeric. Financial cost of failure for a 10kV transformer.
-#' @source DNO Common Network Asset Indices Methodology (CNAIM),
-#' Health & Criticality - Version 2.1, 2021:
+#' @title Consecuencias Ambientales de Falla para Transformadores de 13.2 kV
+#' @description Esta función calcula las consecuencias ambientales de falla
+#' para todos los tipos de transformadores. (cf. sección 7.5, página 84, CNAIM, 2021).
+#' Las consecuencias ambientales de falla se utilizan en
+#' la derivación de las consecuencias de falla, ver \code{\link{cof}}().
+#' @param tipo_transformador Cadena de texto. Tipos de transformadores.
+#' Opciones: \code{tipo_transformador = c("Transformador 13.2kV")}.
+#' @param kva Numérico. La capacidad nominal de un transformador.
+#' Un valor de \code{"Default"} resultará en un
+#' factor ambiental de tamaño de 1 (cf. tabla 230, página 187, CNAIM, 2021).
+#' @param distancia_agua Numérico. Especificar la proximidad a un curso de agua en metros.
+#' Un valor de \code{"Default"} resultará en un factor de proximidad de 1. Por lo tanto,
+#' se asume que la proximidad a un curso de agua está entre 80m y 120m
+#' (cf. tabla 231, página 188, CNAIM, 2021).
+#' @param acotado Cadena de texto. Opciones: \code{acotado = c("Sí", "No", "Default")}.
+#' Un valor de \code{"Default"} resultará en un factor de contención de 1.
+#' @param gb_ref_given parámetro opcional para usar valores de referencia personalizados
+#' @return Numérico. Costo financiero de falla para un transformador de 13.2 kV.
+#' @source Metodología Común de Índices de Activos de Red (CNAIM),
+#' Salud y Criticidad - Versión 2.1, 2021:
 #' \url{https://www.ofgem.gov.uk/sites/default/files/docs/2021/04/dno_common_network_asset_indices_methodology_v2.1_final_01-04-2021.pdf}
 #' @export
 #' @examples
-#' # Environmental consequences of failure for a 6.6/11 kV transformer
-#' e_cof_tf(asset_type_tf = "6.6/11kV Transformer (GM)",
-#' rated_capacity = 750, prox_water = 100, bunded = "Yes")
+#' # Consecuencias ambientales de falla para un transformador de 13.2 kV
+#' e_cof_transformador_13_2kv(tipo_transformador = "Transformador 13.2kV",
+#'                            kva = 750,
+#'                            distancia_agua = 100,
+#'                            acotado = "Sí")
 
-e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
-                     prox_water = "Default", bunded = "Default",
-                     gb_ref_given = NULL) {
+e_cof_transformador_13_2kv <- function(tipo_transformador = "Transformador 13.2kV",
+                                       kva = "Default",
+                                       distancia_agua = "Default",
+                                       acotado = "Default",
+                                       gb_ref_given = NULL) {
 
   `Asset Register Category` = `Health Index Asset Category` = `Lower` = NULL
   # due to NSE notes in R CMD check
@@ -48,7 +45,6 @@ e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
   }
 
   # Get category ------------------------------------------------------------
-
   asset_category <- gb_ref_taken$categorisation_of_assets %>%
     dplyr::filter(`Asset Register Category` == asset_type_tf) %>%
     dplyr::select(`Health Index Asset Category`) %>% dplyr::pull()
@@ -57,11 +53,9 @@ e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
   reference_costs_of_failure <-
     gb_ref_taken$reference_costs_of_failure
 
-
     reference_costs_of_failure_tf <- dplyr::filter(reference_costs_of_failure,
                                                    `Asset Register Category` ==
-                                                     asset_type_tf)
-
+                                                     tipo_transformador)
 
   # Reference environmental cost of failure ---------------------------------
   ecost <- reference_costs_of_failure_tf$`Environmental - (GBP)`
@@ -70,11 +64,11 @@ e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
   type_environmetal_factors <- 1 # Default for assets not in table 221
 
   # Size environmetal factor table 222 --------------------------------------
-  if (asset_type_tf == "132kV Transformer (GM)") {
+  if (tipo_transformador == "132kV Transformer (GM)") {
     size_enviromental_factor_tf <- dplyr::filter(
       gb_ref_taken$size_enviromental_factor,
       `Asset Register Category` ==
-        asset_type_tf)
+        tipo_transformador)
 
     size_enviromental_factor_tf <-
       size_enviromental_factor_tf %>% dplyr::filter(!is.na(Lower))
@@ -84,23 +78,22 @@ e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
     size_enviromental_factor_tf <- dplyr::filter(
       gb_ref_taken$size_enviromental_factor,
       `Asset Register Category` ==
-        asset_type_tf)
+        tipo_transformador)
   }
 
   for (n in 1:nrow(size_enviromental_factor_tf)){
-    if (rated_capacity == 'Default'){
+    if (kva == 'Default'){
       size_environmental_factor <- 1
       break
     } else if (rated_capacity >= as.numeric(
       size_enviromental_factor_tf$Lower[n]) &
-      rated_capacity < as.numeric(
+      kva < as.numeric(
         size_enviromental_factor_tf$Upper[n])){
       size_environmental_factor <-
         size_enviromental_factor_tf$`Size Environmental Factor`[n]
       break
     }
   }
-
 
   # Location environmetal factor table 222 ----------------------------------
   location_environ_al_factor <- gb_ref_taken$location_environ_al_factor
@@ -110,29 +103,29 @@ e_cof_tf <- function(asset_type_tf, rated_capacity = "Default",
                                                    asset_category)
 
   # Bunded "Yes", "No", "Default" ?
-  if (bunded == "Default") {
+  if (acotado == "Default") {
     bunding_factor <- 1
-  } else if (bunded == "Yes") {
+  } else if (acotado == "Sí") {
     bunding_factor <-
       location_environ_al_factor_tf$`Bunding Factor: Bunded`
-  } else if (bunded == "No") {
+  } else if (acotado == "No") {
     bunding_factor <-
       location_environ_al_factor_tf$`Bunding Factor: Not bunded`
   }
 
   # Proximity to water.
-  if(prox_water == "Default") {
+  if(distancia_agua == "Default") {
     prox_factor <- 1
-  } else if (prox_water >= 40 && prox_water < 80) {
+  } else if (distancia_agua >= 40 && distancia_agua < 80) {
     prox_factor <- location_environ_al_factor_tf$
       `Proximity Factor: Close to Water Course (between 40m and 80m)`
-  } else if (prox_water >= 80 && prox_water < 120) {
+  } else if (distancia_agua >= 80 && distancia_agua < 120) {
     prox_factor <- location_environ_al_factor_tf$
     `Proximity Factor: Moderately Close to Water Course (between 80m and 120m)`
-  } else if (prox_water > 120) {
+  } else if (distancia_agua > 120) {
     prox_factor <- location_environ_al_factor_tf$
       `Proximity Factor: Not Close to Water Course (>120m) or No Oil`
-  } else if (prox_water < 40) {
+  } else if (distancia_agua < 40) {
     prox_factor <- location_environ_al_factor_tf$
       `Proximity Factor: Very Close to Water Course (<40m)`
   }
